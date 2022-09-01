0312fa2148df4bfb187f38ab240ce5beb3c71bf2
Aditya Sharma <adityashf7@gmail.com>
2022-09-01 23:49:01 +0530

    Initial commit

---
+++ src/main/java/com/pixel/springqrapp/SpringQrAppApplication.java
@@ -0,0 +1,13 @@
+package com.pixel.springqrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixel/springqrapp/SpringQrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixel.springqrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
